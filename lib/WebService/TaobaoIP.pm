9fa6f06305fee2b75a3bc803acb864c09dae7371
Xiaodong Xu <xxdlhy@gmail.com>
2013-10-21 15:31:20 +0800

    Rewrite accessors

--- lib/WebService/TaobaoIP.pm
+++ lib/WebService/TaobaoIP.pm
@@ -1,8 +1,8 @@
 package WebService::TaobaoIP;
 
-use Moo;
 use utf8;
-use Carp;
+use Moo;
+use Carp qw(croak);
 use JSON::XS;
 use LWP::UserAgent;
 
@@ -11,15 +11,16 @@ use LWP::UserAgent;
 
 binmode STDOUT, ':encoding(UTF8)';
 
-has 'ip' => ( is => 'rw', required => 1 );
-has 'country' => ( is => 'ro' );
-has 'area'    => ( is => 'ro' );
-has 'region'  => ( is => 'ro' );
-has 'city'    => ( is => 'ro' );
-has 'isp'     => ( is => 'ro' );
+for my $attribute (qw(ip country area region city isp)) {
+    if ( $attribute eq 'ip' ) {
+        has $attribute => ( is => 'rw', required => 1 );
+    }
+    else {
+        has $attribute => ( is => 'ro' );
+    }
+}
 
-sub BUILDARGS
-{
+sub BUILDARGS {
     my ( $class, @args ) = @_;
 
     unshift @args, 'ip' if @args % 2 == 1;
@@ -27,8 +28,7 @@ sub BUILDARGS
     return {@args};
 }
 
-sub BUILD
-{
+sub BUILD {
     my $self = shift;
 
     my $base_url = 'http://ip.taobao.com/service/getIpInfo.php?ip=';
@@ -37,16 +37,14 @@ sub BUILD
     my $ua  = LWP::UserAgent->new;
     my $res = $ua->get($full_url);
 
-    if ( $res->is_success )
-    {
+    if ( $res->is_success ) {
         my $info = JSON::XS->new->decode( $res->content );
         $info->{code} == 0
             ? %$self
             = %{ $info->{data} }
             : croak "$self->ip: get information failed.";
     }
-    else
-    {
+    else {
         croak $res->status_line;
     }
 }
